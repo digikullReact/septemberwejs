95cf2612c2fbe70c58683a4325151b8ab252bd36
shubh <shubhamdixit863@gmail.com>
2022-10-29 21:17:30 +0530

    JS initial

---
+++ index.js
@@ -0,0 +1,33 @@
+//variable declarations -->
+// var ,let and const --->
+//[var name=23]  --->variable (the whole line is variable declaration/creation)  
+// var --->keyword which is imposed by js language to use while creating any variable
+// name --- > the name of the variable  or you can call it variable as well
+// 23 --->is the (real world ) data that this variable is storing 
+
+// primitive types 
+// numbers (1,2,3)
+// string (alphabets) "" ,''
+
+//var personName='shivam'
+
+
+// boolean  - true false
+
+//var iwilEattoday= true 
+// undefined ,null ,symbol and bigInt
+
+//keywords ----> are just some words (alphabet words) that are predefined in any programming language
+
+
+var num1=2;
+var num2=3;
+// I want to find their sum -->
+
+var sum =num1+num2;  // it will do addition of numbers 
+
+
+// Techniques to see the output in the browser
+console.log("The sum of two numbers is -"+sum)
+//alert("The sum of two numbers is -"+sum)  + is used for joining the two parts
+//document.write(sum);
