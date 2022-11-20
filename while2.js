7b917fec213e8a6faf0e847095f148d8b35fc3c5
shubh <shubhamdixit863@gmail.com>
2022-11-20 21:36:08 +0530

    added

--- while2.js
+++ while2.js
@@ -100,3 +100,56 @@ sumofNanaturalWithwhile(89);
 
 // You have to write a function that checks whether the number is prime or not
 // you have to print all the prime numbers between 1 - 1000
+  //isPrime(1);
+  
+  // Prime number shoudle have the factors as 1 and itself 
+  // if there is any other factor existing then its a not a prime number
+  
+  //  is a prime number 
+  
+  // 25
+  // 2 3 4 5 6 7 8 9 10 11   ---   24
+  
+  // it shouled not be divisible by any number except 1 and itself
+  
+  
+  // This is a program to find a number is prime or not
+  
+
+
+function isPrime(num){
+    var flagIsprime=true;
+  
+    if(num==1 || num==0){
+      flagIsprime=false;
+      
+    }
+     else{
+
+        // how reduce the complexity of for loop ---> 
+  for(var i=2;i<num;i++){
+     if (num%i==0){
+      flagIsprime=false;
+        break;  // it exits the loop // it exits the complete loop
+        
+        }
+    }
+  
+  
+      
+    }
+    
+  
+  
+  console.log(flagIsprime);
+    
+  }
+  
+  
+  //isPrime(17);
+  //isPrime(89);
+  //isPrime(76);
+  //isPrime(4);
+  
+  isPrime(25);
+  
