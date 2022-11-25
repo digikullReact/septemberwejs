3bd35af1a3a5e5906cdaa2777844375879ad9416
shubh <shubhamdixit863@gmail.com>
2022-11-25 22:30:27 +0530

    sum of n natural numbers

--- functions4.js
+++ functions4.js
@@ -32,3 +32,53 @@ function sumOftwonumbers (a,b) // parameters
     
   }
 sumOftwonumbers(2,3); //arguments
+
+
+// for loop --->
+
+// three things every loop has
+// from where the loop start 
+// where the loop is finishing
+// and how to reach the finish line
+
+//let j=1;
+/*
+for(var a=5; a<55;a++){
+  // we put the code we want to repeat
+
+  console.log("- hello world");
+  //j++
+
+  
+}
+for(var a=55; a>0;a--){
+  // we put the code we want to repeat
+
+  console.log("- hello world");
+  //j++
+
+  
+}
+*/
+
+
+// sum of n natural numbers write the logic
+
+function  sumOfTen() {
+
+    var sum=0; // this is to hold the sum value 
+  
+    for (let i=1;i<=10; i++){
+      
+       sum=sum+i              // reassignment
+      
+    }
+    console.log(sum);
+    
+  }
+  sumOfTen() ;
+
+
+
+
+
