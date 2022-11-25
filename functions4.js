6db3033a5af891a861a3801afe5dc2e64a5f0507
shubh <shubhamdixit863@gmail.com>
2022-11-25 21:51:12 +0530

    functions

---
+++ functions4.js
@@ -0,0 +1,34 @@
+
+// function definition
+function fnname ()
+  {
+     // Goes your logic that you want to run 
+
+    var a=9;
+var b=10;
+var sum=a+b;
+console.log(sum);
+
+    
+  }
+
+
+
+// function call -->here -->
+ fnname();
+
+
+// parameters and arguments --->
+
+// parameters
+
+function sumOftwonumbers (a,b) // parameters
+  {
+    
+   var sum=a+b;
+  console.log(sum);
+    
+    
+    
+  }
+sumOftwonumbers(2,3); //arguments
