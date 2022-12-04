0b638cdbebfb174a33ad6b8a8dec46c11bb3c75a
shubh <shubhamdixit863@gmail.com>
2022-12-04 21:02:03 +0530

    intro to dom

---
+++ dom.js
@@ -0,0 +1,11 @@
+let buttonelement=document.getElementById("btn"); // I selected the button html element
+
+buttonelement.addEventListener("click",function(){
+    //alert("button clicked")
+    let htmlelement=document.getElementById("heading");
+htmlelement.style.color="green";
+
+
+})
+
+
