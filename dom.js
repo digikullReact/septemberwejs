4095905d1c72a40e471a0b4e03a4ac64ba538105
shubh <shubhamdixit863@gmail.com>
2022-12-04 21:20:18 +0530

    event click

--- dom.js
+++ dom.js
@@ -1,11 +1,21 @@
 let buttonelement=document.getElementById("btn"); // I selected the button html element
+let buttonelement2=document.getElementById("btn2"); // I selected the button html element
 
+let htmlelement=document.getElementById("heading");
 buttonelement.addEventListener("click",function(){
     //alert("button clicked")
-    let htmlelement=document.getElementById("heading");
+   
 htmlelement.style.color="green";
 
 
 })
 
 
+buttonelement2.addEventListener("click",function(){
+    //alert("button clicked")
+   
+htmlelement.style.fontFamily="verdana";
+
+
+})
+// create another button that changes the font family of the heading on its click
